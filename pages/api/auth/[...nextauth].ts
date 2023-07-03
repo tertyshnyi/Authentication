1fa339d28c413e4eedcfef2e31a61cbfcf49e1d9
tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2023-07-03 20:55:27 +0200

    updated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,58 +1,65 @@
-import { compare } from 'bcryptjs'
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import { connectToMongoDB } from '../../../database/mongodb'
-import User from '../../../models/user'
-import { IUser } from '../../../interfaces'
+require('dotenv').config(); // Load environment variables from .env file
+import { compare } from 'bcryptjs';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { connectToMongoDB } from '../../../database/mongodb';
+import User from '../../../models/user';
+import { IUser } from '../../../interfaces';
 
 const options: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                await connectToMongoDB().catch(err => { throw new Error(err) })
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connectToMongoDB().catch((err) => {
+          throw new Error(err);
+        });
 
-                const user = await User.findOne({
-                    email: credentials?.email
-                }).select("+password")
+        const user = await User.findOne({
+          email: credentials?.email,
+        }).select("+password");
 
-                if (!user) {
-                    throw new Error("Invalid credentials")
-                }
-
-                const isPasswordCorrect = await compare(credentials!.password, user.password)
-
-                if (!isPasswordCorrect) {
-                    throw new Error("Invalid credentials")
-                }
-
-                return user
-            }
-        })
-    ],
-    pages: {
-        signIn: "/login"
-    },
-    session: {
-        strategy: "jwt"
-    },
-    callbacks: {
-        jwt: async ({ token, user }) => {
-            user && (token.user = user)
-            return token
-        },
-        session: async ({ session, token }) => {
-            const user = token.user as IUser
-            session.user = user
-
-            return session
+        if (!user) {
+          throw new Error("Invalid credentials");
         }
-    }
-}
 
-export default NextAuth(options)
+        const isPasswordCorrect = await compare(
+          credentials!.password,
+          user.password
+        );
+
+        if (!isPasswordCorrect) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      user && (token.user = user);
+      return token;
+    },
+    session: async ({ session, token }) => {
+      const user = token.user as IUser;
+      session.user = user;
+
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET, // Use environment variable for secret key
+};
+
+export default NextAuth(options);
